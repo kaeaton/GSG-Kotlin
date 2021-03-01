125e49638f178570ed839e1ae9f53952764c105c
Katrina Eaton <katrina.eaton@ucsf.edu>
2021-03-01 11:14:13 -0800

    Restructure into microkernal architecture

--- src/main/kotlin/org/b12x/gfe/MyApp.kt
+++ src/main/kotlin/org/b12x/gfe/MyApp.kt
@@ -1,6 +1,6 @@
 package org.b12x.gfe
 
-import org.b12x.gfe.view.MainView
+import org.b12x.gfe.core.view.MainView
 import tornadofx.App
 
 class MyApp: App(MainView::class, Styles::class)
---
+++ src/main/kotlin/org/b12x/gfe/plugins/gfesearch/view/GfeSearchSubmitButton.kt
@@ -0,0 +1,22 @@
+package org.b12x.gfe.plugins.gfesearch.view
+
+import tornadofx.*
+import javafx.scene.control.Button
+
+class GfeSearchSubmitButton : View("Submit") {
+    private val resultsTextField = textfield("Nothing to display")
+
+    override val root = vbox {
+        button("Update text") {
+            action {
+                runAsync {
+//                  myController.loadText()
+                    setOnAction { println("You pressed the GFE Search submit button.") }
+                } ui {
+                    resultsTextField.text = "Data submitted"
+                }
+            }
+        }
+        add(resultsTextField)
+    }
+}
