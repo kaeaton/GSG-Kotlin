8fac900440fb03b730cfdaf87f9fc6ca4280d9ef
Katrina Eaton <katrina.eaton@ucsf.edu>
2021-07-02 15:57:04 -0700

    create test app

--- src/test/kotlin/org/b12x/gfe/plugins/gfesearch/view/TestApp.kt
+++ src/test/kotlin/org/b12x/gfe/plugins/gfesearch/view/TestApp.kt
@@ -12,8 +12,16 @@ import tornadofx.*
 import java.util.Collections.list
 
 class TestApp: App(TestLayout::class, Styles::class) {
+    val parentStage = Stage()
+
+//    override fun onDock() {
+//        runAsync {
+//                Thread.sleep(100)
+//            }
+//        }
+
     override fun start(stage: Stage) {
-        super.start(stage)
+        super.start(parentStage)
         stage.width = 400.0
         stage.height = 300.0
     }
@@ -48,7 +56,7 @@ class TestSearchBox : View() {
 
     val set2 = vbox {
         var check2 = checkbox { }
-        var text2 = textfield { }
+        var text2 = textfield {"4"}
 
         checkBoxes.add(check2)
         textFields.add(text2)
