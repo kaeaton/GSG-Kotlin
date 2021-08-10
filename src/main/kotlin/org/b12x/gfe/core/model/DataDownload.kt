0ca980b9206c2d6614815afacbf9663b2576a2f5
Katrina Eaton <katrina.eaton@ucsf.edu>
2021-08-10 03:42:10 -0700

    Download version data

--- src/main/kotlin/org/b12x/gfe/core/model/DataDownload.kt
+++ src/main/kotlin/org/b12x/gfe/core/model/DataDownload.kt
@@ -1,17 +1,25 @@
 package org.b12x.gfe.core.model
 
-import okhttp3.*
+//import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import java.net.URL
+import javax.json.Json
+
+//import java.net.URL
 
 class DataDownload {
     private val MEDIA_TYPE_JSON = "application/json; charset=utf-8".toMediaType()
-    private val DB_VERSIONS = "http://gfe.b12x.org/imgt-versions"
+    private val DB_VERSIONS = "http://gfe.b12x.org/v1/imgt-versions"
 
     fun makeRequest(url: String, request: String): String {
         val okHttpClient = OkHttpClient()
-        val parsedResponse = parseResponse(okHttpClient.newCall(createRequest(url, request)).execute())
-        return parsedResponse
+//        val parsedResponse = parseResponse(okHttpClient.newCall(createRequest(url, request)).execute())
+        return parseResponse(okHttpClient.newCall(createRequest(DB_VERSIONS, request)).execute())
     }
 
     fun createRequest(url: String, request: String): Request {
--- src/main/kotlin/org/b12x/gfe/main.kt
+++ src/main/kotlin/org/b12x/gfe/main.kt
@@ -1,8 +1,17 @@
 package org.b12x.gfe
 
+import org.b12x.gfe.core.model.DataDownload
+import org.b12x.gfe.core.model.JsonParser
 import tornadofx.launch
 
 fun main() {
-    launch<MyApp>()
+//    launch<MyApp>()
+
+    val dataDownload = DataDownload()
+//    val jsonParser = JsonParser()
+//    print("{${jsonParser.parseJson(dataDownload.makeRequest("",""))}}")
+    val versions = dataDownload.makeRequest("","").toString()
+    print(versions)
 }
+
 
