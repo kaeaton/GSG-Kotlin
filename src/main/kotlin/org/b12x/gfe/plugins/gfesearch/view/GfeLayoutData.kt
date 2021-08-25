a0aeefd9d49f9a7be98cbbc9ccdc662461e42b39
Katrina Eaton <katrina.eaton@ucsf.edu>
2021-08-25 13:44:25 -0700

    Convert to Java 11, stack overflows seem to have stopped

--- src/main/kotlin/org/b12x/gfe/plugins/gfesearch/view/GfeLayoutData.kt
+++ src/main/kotlin/org/b12x/gfe/plugins/gfesearch/view/GfeLayoutData.kt
@@ -22,27 +22,14 @@ class GfeLayoutData {
         var locusName: String = "HLA-DRB1"
 
         fun resetArraysHard() {
-            println("CheckArray size before reset: ${checkList.size}")
-            println("TextArray size before reset: ${textList.size}")
             checkList = ArrayList()
-            println("CheckArray size after reset: ${checkList.size}")
             textList = ArrayList()
-            println("TextArray size after reset: ${textList.size}")
         }
 
         fun resetArraysSoft() {
-            runAsync {
-
-                checkList.forEach { it.isSelected = false }
-
-//                println("TextArray size before reset: ${textList.size}")
-                textList[0].textProperty().value = "w"
-                for (i in 1 until textList.size) {
-                    textList[i].textProperty().value = ""
-                }
-//                println("TextArray size after reset: ${textList.size}")
-
-            }
+            checkList.forEach { it.isSelected = false }
+            textList.forEach { it.textProperty().value = "" }
+            textList[0].textProperty().value = "w"
         }
     }
 }
