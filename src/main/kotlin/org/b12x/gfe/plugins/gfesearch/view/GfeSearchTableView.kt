e3d1f1563bb9a8d07301fc6802bb62e8a6734f4e
Katrina Eaton <EatonK@peds-00lk7gf-sw.ucsf.edu>
2022-01-13 16:47:17 -0800

    Add new table example

--- src/main/kotlin/org/b12x/gfe/plugins/gfesearch/view/GfeSearchTableView.kt
+++ src/main/kotlin/org/b12x/gfe/plugins/gfesearch/view/GfeSearchTableView.kt
@@ -1,6 +1,9 @@
 package org.b12x.gfe.plugins.gfesearch.view
 
+import javafx.beans.property.ReadOnlyStringWrapper
+import javafx.scene.control.TreeItem
 import tornadofx.*
+import tornadofx.column
 
 class GfeSearchTableView(dataFormat: String): View() {
 
@@ -10,21 +13,52 @@ class GfeSearchTableView(dataFormat: String): View() {
         GfeSearchResult("HLA-A*01:01:01:03", "HLA-Aw2-1-2-1-1-1-1-1-1-1-1-1-1-1-1-1-4")
     ).asObservable()
 
+    val tableData = mapOf(
+        "Fruit" to arrayOf("apple", "pear", "Banana"),
+        "Veggies" to arrayOf("beans", "cauliflower", "cale"),
+        "Meat" to arrayOf("poultry", "pork", "beef")
+    )
+
     var testname = data[0].gfeName.split("-")
 
-    override val root = hbox {
+//    var prettyTableView = tableview(data) {
+//        readonlyColumn("Name", GfeSearchResult::name)
+//        for (i in 0 until data[0].gfeName.split(",").size) {
+//            column<String, String>("test $i", { it.value})
+//            populate {
+//                ReadOnlyStringWrapper(it.value)
+//
+//            }
+//        }
+//    }
+
+
+        override val root = hbox {
         tableview(data) {
             readonlyColumn("Name", GfeSearchResult::name)
             readonlyColumn("GFE", GfeSearchResult::gfeName)
         }
-        add(prettyTableView)
-    }
-
-    var prettyTableView = tableview(data) {
-        for (i in 0 until data[0].gfeName.split(",").size) {
-
+        treetableview<String>(TreeItem("Items")) {
+            column<String, String>("Type", { it.value.valueProperty() })
+            populate {
+                if (it.value == "Items") tableData.keys
+                else tableData[it.value]?.asList()
+            }
         }
+//        add(prettyTableView)
     }
+
+
+
+
+//    val example = treetableview<String>(TreeItem("Items")) {
+//        column<String, String>("Type", { it.value.valueProperty() })
+//        populate {
+//            if (it.value == "Items") tableData.keys
+//            else tableData[it.value]?.asList()
+//        }
+//    }
+
 }
 
 data class GfeSearchResult(val name: String, val gfeName: String)
