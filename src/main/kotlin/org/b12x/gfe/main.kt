18e90e57da02b1b5cd75a85378e7166eec2c4e0a
Katrina Eaton <katrina.eaton@ucsf.edu>
2022-02-15 08:45:41 -0800

    clean up main.kt

--- src/main/kotlin/org/b12x/gfe/main.kt
+++ src/main/kotlin/org/b12x/gfe/main.kt
@@ -6,21 +6,10 @@ import java.io.File
 
 
 fun main() {
-
     InternetAccess.internetAccess = InternetAccess.isInternetAvailable()
     println("Internet access? " + InternetAccess.internetAccess)
 
     launch<GSG>()
-
-
-//    val notEmptyFolder = File("/Users/EatonK/Documents/GSG/GSGData/HLA/2.0.0/")
-//    val emptyFolder = File("/Users/EatonK/Documents/GSG/GSGData/HLA/2.0.1/")
-//    println(notEmptyFolder.list().size)//.list().toString())
-//    println(notEmptyFolder.list().isNotEmpty())//.list().toString())
-//
-//    println(emptyFolder.list().size)
-//    println(emptyFolder.list().isNotEmpty())//.list().toString())
-
 }
 
 
