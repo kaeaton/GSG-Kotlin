a555e50ca4c1675a90941046fd70582c8c2dd7ec
Katrina Eaton <katrina.eaton@gmail.com>
2023-09-09 07:53:01 -0700

    Move App class into mainKt

--- src/main/kotlin/org/b12x/gfe/GSG.kt
+++ src/main/kotlin/org/b12x/gfe/GSG.kt
@@ -6,20 +6,20 @@ import org.b12x.gfe.core.view.MainView
 import org.b12x.gfe.utilities.InternetAccess
 import tornadofx.*
 
-class GSG : App(MainView::class, Styles::class) {
-
-    private val gsgWidth = 1150.0
-    private val gsgHeight = 800.0
-
-    override fun start(stage: Stage) {
-        super.start(stage)
-        stage.width = gsgWidth
-        stage.height = gsgHeight
-//        stage.setOnHiding { stop() }
-
-        /* Internet Access */
-        val internetAccess = InternetAccess()
-        internetAccess.printResults()
-    }
-}
+//class GSG : App(MainView::class, Styles::class) {
+//
+//    private val gsgWidth = 1150.0
+//    private val gsgHeight = 800.0
+//
+//    override fun start(stage: Stage) {
+//        super.start(stage)
+//        stage.width = gsgWidth
+//        stage.height = gsgHeight
+////        stage.setOnHiding { stop() }
+//
+//        /* Internet Access */
+//        val internetAccess = InternetAccess()
+//        internetAccess.printResults()
+//    }
+//}
 
--- src/main/kotlin/org/b12x/gfe/main.kt
+++ src/main/kotlin/org/b12x/gfe/main.kt
@@ -1,16 +1,33 @@
 package org.b12x.gfe
 
+import javafx.stage.Stage
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.b12x.gfe.core.controller.PrefsCore
 import org.b12x.gfe.core.model.datadownload.version.DownloadVersion
+import org.b12x.gfe.core.view.MainView
 import org.b12x.gfe.plugins.gfesearch.controller.locistategfesearch.PrefsGfeSearch
 import org.b12x.gfe.utilities.InternetAccess
 import tornadofx.*
 
+class GSG : App(MainView::class, Styles::class) {
 
+    private val gsgWidth = 1150.0
+    private val gsgHeight = 800.0
+
+    override fun start(stage: Stage) {
+        super.start(stage)
+        stage.width = gsgWidth
+        stage.height = gsgHeight
+//        stage.setOnHiding { stop() }
+
+        /* Internet Access */
+        val internetAccess = InternetAccess()
+        internetAccess.printResults()
+    }
+}
 fun main() {
 
     /* Prefs Reset */
